09ff7bf9ee8eb4ab9923725d348405ffb712f459
RSooriya <soormo06@gmail.com>
2020-06-28 14:21:34 +0530

    Commit made for Constructor dependency injection

---
+++ src/com/sooriya/BaseballCoach.java
@@ -0,0 +1,21 @@
+package com.sooriya;
+
+public class BaseballCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	public BaseballCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice batting for daily 30 mins";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
