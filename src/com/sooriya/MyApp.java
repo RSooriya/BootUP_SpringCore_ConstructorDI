7accfa400540baf31fa35ed47500988c1adceca4
RSooriya <soormo06@gmail.com>
2020-06-30 13:27:12 +0530

    Comments added

--- src/com/sooriya/MyApp.java
+++ src/com/sooriya/MyApp.java
@@ -6,9 +6,14 @@ public class MyApp {
 
 	public static void main(String[] args) {
 
+		// Creating spring container and loading configuration file into container
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieving beans from container
 		Coach theCoach = context.getBean("myCoach",Coach.class);
+		
+		//calling methods on bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
 		context.close();
